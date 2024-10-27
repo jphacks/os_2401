0831937331a745542f630881eb0cb6a26b19ccba
shimizu <noon3m.op.sh@gmail.com>
2024-10-27 13:47:58 +0900

    [add]カットインのbgm追加

--- front/src/pages/user/animation/Animation.tsx
+++ front/src/pages/user/animation/Animation.tsx
@@ -6,15 +6,46 @@ import standup from "../animation/anime_img/standup.png";
 import clap from "../animation/anime_img/clap.gif";
 import hand_up from "../animation/anime_img/hand_up.gif";
 
+import cat from "../animation/anime_img/cut-in.mp3";
+
+let catIn: HTMLAudioElement | null = null;
+
 export const Animation = ({ arg }: { arg: number }) => {
   const [isVisible, setIsVisible] = useState(true);
 
   const img = ["", no_objection, objection, standup, clap, hand_up];
 
+  const bgmPlayer = async () => {
+    try {
+      if (catIn) {
+        // 既存の音声が再生中であれば一度停止
+        catIn.pause();
+        catIn = null;
+      }
+
+      catIn = new Audio(cat);
+
+      catIn.addEventListener("ended", () => {
+        catIn = null;
+        // サイド再生する
+        catIn = new Audio(cat);
+      });
+
+      await catIn.play();
+    } catch (error) {
+      console.error("Failed to fetch or play audio:", error);
+    }
+  };
+
   useEffect(() => {
+    bgmPlayer();
     setIsVisible(true); // コンポーネントが更新されたときにアニメーションを表示状態にリセット
     const timer = setTimeout(() => {
       setIsVisible(false); // アニメーションを非表示にする
+      if (catIn) {
+        catIn.pause();
+        catIn = null;
+      }
     }, 2000); // アニメーションの時間を2秒に変更
 
     return () => clearTimeout(timer);
